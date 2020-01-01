9de54cf6ee6f4fac74215fbeff370bd08161e924
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2020-01-01 00:09:06 +0000

    Change the form into a card, add JS
    This commit changes the style of the form to a card to match the style
    of the index page. It also adds JS to show or hide the new group box
    depending on the value of the select box.

--- hkweb/hkweb/app/assets/javascripts/landings.js
+++ hkweb/hkweb/app/assets/javascripts/landings.js
@@ -1,25 +1,21 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function() {
-  $(".toggle-accordion").on("click", function() {
-    var accordionId = $(this).attr("accordion-id"),
-      numPanelOpen = $(accordionId + ' .collapse.in').length;
+  var $selector = $(".js-select-group");
+  var $selectorInput = $(".js-new-group-input");
 
-      $(this).toggleClass("active");
+  toggleNewGroupInput($selector, $selectorInput);
 
-      if (numPanelOpen == 0) {
-        openAllPanels(accordionId);
-      } else {
-        closeAllPanels(accordionId);
-      }
-    })
+  $selector.on("change", toggleNewGroupInput);
 
-  openAllPanels = function(aId) {
-    console.log("setAllPanelOpen");
-    $(aId + ' .panel-collapse:not(".in")').collapse('show');
-  }
-  closeAllPanels = function(aId) {
-    console.log("setAllPanelclose");
-    $(aId + ' .panel-collapse.in').collapse('hide');
-  }
 });
+
+function toggleNewGroupInput($selector, $selectorInput) {
+  if ($selector.val() === "") {
+    $selectorInput.show();
+  }
+  else
+  {
+    $selectorInput.hide();
+  }
+}
